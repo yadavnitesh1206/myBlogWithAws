a590930b577587e4a9edd4a98238ea2e61710bcd
nitesh <nitesh@gmail.com>
2026-05-26 16:53:37 +0530

    updated for converting war

--- src/main/java/com/myBlogPost/MyBlogPostApplication.java
+++ src/main/java/com/myBlogPost/MyBlogPostApplication.java
@@ -3,10 +3,18 @@ package com.myBlogPost;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyBlogPostApplication {
+public class MyBlogPostApplication extends SpringBootServletInitializer {
+
+//	added by me for war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyBlogPostApplication.class);
+	}
 
 	@Bean
 	public ModelMapper modelMapper(){
