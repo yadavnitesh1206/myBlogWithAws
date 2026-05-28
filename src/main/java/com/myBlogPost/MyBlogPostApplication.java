e5b4267c3d8947c1d23e1718e1b740e5162056ce
nitesh <nitesh@gmail.com>
2026-05-28 14:35:32 +0530

    added war for test on ubuntu

--- src/main/java/com/myBlogPost/MyBlogPostApplication.java
+++ src/main/java/com/myBlogPost/MyBlogPostApplication.java
@@ -3,16 +3,22 @@ package com.myBlogPost;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyBlogPostApplication {
+public class MyBlogPostApplication extends SpringBootServletInitializer {
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyBlogPostApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogPostApplication.class, args);
 	}
